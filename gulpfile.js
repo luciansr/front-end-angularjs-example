ecd4e487a958ed4858c7cb27ea26fd2c96109fe4
Lucian Sturião Rodrigues <luciansr@gmail.com>
2017-10-20 00:24:53 -0200

    gulp-tsc installed

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
+var typescript = require('gulp-tsc');
 
 gulp.task("default", function () {
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest("dist"));
+    // return tsProject.src()
+    //     .pipe(tsProject());
+        //.js.pipe(gulp.dest("dist"));
+
+        gulp.src(['src/**/*.ts'])
+        .pipe(typescript())
+        .pipe(gulp.dest('src/'))
 });
