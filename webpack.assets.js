bce12764ec4c1a2a661de1ca437cc37007296c96
Lucian Sturião Rodrigues <luciansr@gmail.com>
2017-10-25 00:27:56 -0200

    adding font-awesome and adding yarn.lock to git, remaking webpack.config.js to support fonts

--- webpack.assets.js
+++ webpack.assets.js
@@ -8,7 +8,19 @@ const JS = [
     'angular/angular.min.js',
     'angular-route/angular-route.min.js',
     'toastr/build/toastr.min.js',
-    'angular-smart-table/dist/smart-table.min.js'
+    'angular-smart-table/dist/smart-table.min.js',
+    'font-awesome/css/font-awesome.min.css'
 ];
-
-module.exports = [...JS, ...CSS];
+const FONTS = [
+    'font-awesome/fonts/fontawesome-webfont.eot',
+    'font-awesome/fonts/fontawesome-webfont.svg',
+    'font-awesome/fonts/fontawesome-webfont.ttf',
+    'font-awesome/fonts/fontawesome-webfont.woff',
+    'font-awesome/fonts/fontawesome-webfont.woff2',
+    'font-awesome/fonts/FontAwesome.otf',
+];
+module.exports = {
+    "JS" : JS,
+    "CSS" : CSS,
+    "FONTS" : FONTS
+};
--- webpack.config.js
+++ webpack.config.js
@@ -15,10 +15,18 @@ module.exports = {
     },
     plugins: [
       new CopyWebpackPlugin(
-        Assets.map(asset => {
+        [...Assets.JS,...Assets.CSS].map(asset => {
           return {
             from: path.resolve(__dirname, `./node_modules/${asset}`),
             to: path.resolve(__dirname, './src/libs')
+          };
+        })
+      ),
+      new CopyWebpackPlugin(
+        Assets.FONTS.map(asset => {
+          return {
+            from: path.resolve(__dirname, `./node_modules/${asset}`),
+            to: path.resolve(__dirname, './src/libs/fonts')
           };
         })
       )
